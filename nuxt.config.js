a2b62bb9c65e558ce7c808279465422bbf58006e
zhaohaitao <zachzhao@soarinfotech.com>
2022-10-25 10:24:34 +0800

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,5 +46,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  server: {
+    port: 4001, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
 }
