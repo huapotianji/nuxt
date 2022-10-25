631e38ed4600c95eac9915376c084db7cf0e4906
zhaohaitao <zachzhao@soarinfotech.com>
2022-10-25 10:20:30 +0800

    配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,9 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:3010',
+    baseURL: 'http://127.0.0.1:3010',//测试
+    // baseURL: 'http://127.0.0.1:3010',//预发
+    // baseURL: 'http://127.0.0.1:3010',//生产
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
