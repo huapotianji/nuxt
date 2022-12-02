ac806aa6858bb10d3aff70478ef3d256262bf358
zhaohaitao <zachzhao@soarinfotech.com>
2022-12-02 11:11:02 +0800

    video

--- pages/video/index.vue
+++ pages/video/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="video">
+    <video autoplay muted loop :src="require('@/assets/video/one.mp4')"></video>
+  </div>
 </template>
 <script>
 export default {
@@ -7,7 +9,32 @@ export default {
   data() {
     return {};
   },
+  head(){
+     let metaConfig = {
+      link: [
+        {
+          rel: 'preload',
+          href: '@/assets/video/one.mp4',
+          as:'script'
+        },
+      ],
+    }
+    return metaConfig
+  }
 };
 </script>
 <style lang='scss' scoped>
+.video{
+    width: 100vw;
+    height: 100vh;
+    background: #fff;
+    overflow: hidden;
+    video{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center center;
+        object-fit: fill;
+    }
+}
 </style>
