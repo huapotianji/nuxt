c8c0924a99b23034bd120c8c6951ed1f2da26a30
zhaohaitao <zachzhao@soarinfotech.com>
2023-07-21 18:22:35 +0800

    host

---
+++ pages/host/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="host">
+    <h1>主机电脑测评及价格</h1>
+    <div class="table">
+        
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='scss' scoped>
+.host {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.table {
+  border: 1px solid #9f9d9d;
+}
+</style>
