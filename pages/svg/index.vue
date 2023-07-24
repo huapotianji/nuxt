9e4f2a1d37fe9096052124fa85787773776cf54b
zhaohaitao <zachzhao@soarinfotech.com>
2023-07-24 16:38:22 +0800

    svg

---
+++ pages/svg/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      src="https://www.hotdeals.com/_nuxt/img/icon_top.8b1420b.svg"
+      alt=""
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='scss' scoped>
+img {
+  width: 72px;
+  height: 72px;
+}
+</style>
